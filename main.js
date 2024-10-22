edba9d41944afa51a21c371c7c97b54d1b670afa
Vaguerpuma <s.munguia001@ebc.edu.mx>
2024-10-22 08:15:43 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+let a = document.querySelector(".button1");
+let b = document.querySelector(".button2");
+let c = document.querySelector(".button3");
+let checo = document.querySelector("#checo");
+let noSirve = document.querySelectorAll(".sidebar_img");
+let d = document.querySelector(".column-2");
+let e = document.querySelector(".Messages");
+let f = document.querySelector(".History");
+let g = document.querySelector(".Tasks");
+let h = document.querySelector(".Communities");
+let i = document.querySelector(".Settings");
+let j = document.querySelector(".Support");
+let k = document.querySelector(".Privacy");
+
+
+
+a.addEventListener("click",() => {
+    alert("No funciona");
+})
+b.addEventListener("click",() => {
+    alert("Este tampoco");
+})
+c.addEventListener("click",() => {
+    alert("Este menos");
+})
+checo.addEventListener("click",() => {
+    alert("Checo es GEI");
+})
+
+for (let i = 0;i < noSirve.length;i++) {
+    let theThirdExample = noSirve[i];
+    theThirdExample.addEventListener("click",() => {
+    alert("Esrto nunca sirvio");
+    })
+}
+d.addEventListener("click",() => {
+    alert("No funciona");
+})
+e.addEventListener("click",() => {
+    alert("No funciona");
+})
+f.addEventListener("click",() => {
+    alert("No funciona");
+})
+g.addEventListener("click",() => {
+    alert("No funciona");
+})
+h.addEventListener("click",() => {
+    alert("No funciona");
+})
+i.addEventListener("click",() => {
+    alert("No funciona");
+})
+j.addEventListener("click",() => {
+    alert("No funciona");
+})
+k.addEventListener("click",() => {
+    alert("No funciona");
+})
